2a0fb7c35dbec3806193570184a96e2c00a77412
Amira Abd ELgawad <amiraabdelgawad97@gmail.com>
2020-12-30 21:12:48 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$("#navbar ul li a, .btn").on("click", function(event) {
+if(this.hash !== ""){
+    event.preventDefault();
+    const hash =this.hash;
+    console.log(hash);
+    $("html, body").animate({scrollTop: $(hash).offset().top -100},800);
+    }
+});
+
+window.addEventListener("scroll",function(){
+    if(window.scrollY >150){
+        document.querySelector("#navbar").style.opacity =0.8;    
+    }
+    else{
+        document.querySelector("#navbar").style.opacity =1;    
+
+    }
+});
